6017e21f63a5b0a807da00c9aff422cac48f1785
reztip <alexpitzer42@gmail.com>
2016-07-10 11:43:42 -0700

    split list unit tests into two files

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,7 +6,6 @@ from lists.models import Item, List
 
 from lists.views import home_page
 
-# Create your tests here.
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -19,41 +18,7 @@ class HomePageTest(TestCase):
         expected_html = render_to_string('home.html', {})
         # currently the csrf token is being added to the expected_html
         # self.assertEqual(response.content.decode(), expected_html)
-
-
-
-
         
-class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list =  list_
-        second_item.save()
-
-        saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
-
-        saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, 'Item the second')
-        self.assertEqual(second_saved_item.list, list_)
-
-        
-
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
@@ -126,5 +91,4 @@ class NewItemTest(TestCase):
         correct_list = List.objects.create()
         response = self.client.get('/lists/{0}/'.format(str(correct_list.id)))
         self.assertEqual(response.context['list'], correct_list)
-
 
